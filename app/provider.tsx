ff4f6fc39fac542e741876861ed7bdd13b6f0590
Dipesh <dipesh.chaulagain@asparksys.com>
2024-06-24 13:04:54 +0545

    Map complete

--- app/provider.tsx
+++ app/provider.tsx
@@ -1,21 +1,17 @@
 "use client"
 import { NextUIProvider } from "@nextui-org/react"
-import { APIProvider } from "@vis.gl/react-google-maps"
+
 import { AppProgressBar as ProgressBar } from "next-nprogress-bar"
 export function Providers({ children }: { children: React.ReactNode }) {
   return (
     <NextUIProvider>
-      <APIProvider
-        apiKey={"AIzaSyA7982Kq5E_xuOmtJR-UpbCZmfOfyu4b7Q"}
-        onLoad={() => console.log("Maps API has loaded.")}>
-        <ProgressBar
-          height="4px"
-          color="#9F2F35"
-          options={{ showSpinner: true }}
-          shallowRouting
-        />
-        {children}
-      </APIProvider>
+      <ProgressBar
+        height="4px"
+        color="#9F2F35"
+        options={{ showSpinner: true }}
+        shallowRouting
+      />
+      {children}
     </NextUIProvider>
   )
 }
